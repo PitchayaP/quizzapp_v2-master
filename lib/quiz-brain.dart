1285bdc4834fa48a067c98e39a69208471b19368
Xie <xie@Ninas-MacBook-Air-2.local>
2022-01-24 14:52:01 +0700

    Initial commit

---
+++ lib/quiz-brain.dart
@@ -0,0 +1,41 @@
+import 'question.dart';
+class QuizBrain {
+
+  int _questionNumber = 0;
+
+  //TODO 1: ปรับเปลี่ยนหรือเพิ่มชุดคำถามใน questionBank ให้มีอย่างน้อย 5 คำถาม
+  List<Question> _questionBank=[
+    Question(q: '1 + 1 = 2', a: true),
+    Question(q: '2 x 4 = 7', a: false),
+    Question(q: '1 + 1 = 3', a: false),
+    Question(q: '1 / 5 = 0.2', a: true),
+    Question(q: '25 = 5 x 5', a: true),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length-1) {
+      _questionNumber++;
+    }
+  }
+
+  String? getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool? getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+
+}
